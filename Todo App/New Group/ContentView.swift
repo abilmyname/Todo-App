972cec751bfdb855d8c39a1b23a93aced2ea79fa
abilmyname <abilnabilachmad@gmail.com>
2021-02-17 11:02:51 +0700

    Perubahan ke #3 (Button Edit,  Delete)

--- Todo App/New Group/ContentView.swift
+++ Todo App/New Group/ContentView.swift
@@ -11,29 +11,60 @@ struct ContentView: View {
   
   @Environment(\.managedObjectContext) var managedObjectContext
   
+  @FetchRequest(entity: Todo.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Todo.name, ascending: true)]) var
+    todos: FetchedResults<Todo>
   @State private var showingAddTodoView: Bool = false
+  
   var body: some View {
     NavigationView{
-      List(0..<5){ item in
-        Text("Hello, world!")
+      List{
+        ForEach(self.todos, id: \.self){ todo in
+          HStack{
+            Text(todo.name ?? "Unknown")
+            
+            Spacer()
+            
+            Text(todo.priority ?? "Unknown")
+          }
+        }
+        .onDelete(perform: deleteTodo)
       }
       .navigationBarTitle("Todo", displayMode: .inline)
-      .navigationBarItems(trailing:
+      .navigationBarItems(leading: EditButton(),
+                          trailing:
                             Button(action: {
                               self.showingAddTodoView.toggle()
                             }){
                               Image(systemName: "plus")
                             }
-        .sheet(isPresented: $showingAddTodoView){
-          AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
-        }
+                            .sheet(isPresented: $showingAddTodoView){
+                              AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
+                            }
       )
+    }
+  }
+  
+  // For Deleting Item
+  private func deleteTodo(at offset: IndexSet){
+    for index in offset{
+      let todo = todos[index]
+      managedObjectContext.delete(todo)
+      
+      do{
+        try managedObjectContext.save()
+      } catch{
+        print(error)
+      }
     }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    return ContentView()
+      .environment(\.managedObjectContext, context)
   }
 }
